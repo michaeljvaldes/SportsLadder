c0ec073b1f43dabf65997c79e90a1e2a9575b25c
Felipe Leite <felipe.leite@ul.com>
2017-07-15 13:00:16 -0400

    Moved the backend code to the backend-sports-ladder folder and made the prefix /api necessary for all of the urls in the backend

---
+++ backend-sports-ladder/src/main/java/com/sportsladder/config/DispatcherServletCustomConfiguration.java
@@ -0,0 +1,27 @@
+package com.sportsladder.config;
+
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+/**
+ * Created by Felipe Leite on 7/15/2017.
+ */
+
+@Configuration
+public class DispatcherServletCustomConfiguration {
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                dispatcherServlet(), "/api/*");
+        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+        return registration;
+    }
+}
